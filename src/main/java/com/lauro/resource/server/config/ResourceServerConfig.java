74ca1d6c0d07b5f4e841926cc69fc45756743338
Lauro Correia Silveira <lauro.silveira@outlook.com.br>
2024-01-23 17:01:01 +0100

    Disable CSRF

--- src/main/java/com/lauro/resource/server/config/ResourceServerConfig.java
+++ src/main/java/com/lauro/resource/server/config/ResourceServerConfig.java
@@ -20,6 +20,7 @@ public class ResourceServerConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .csrf(csrf -> csrf.configure(csrf.disable()))
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.decoder(JwtDecoders.fromOidcIssuerLocation(issuerUri))))
                 .build();
     }
