e067364f160f980cd766e49b4fe7d7537ac3c9fc
Lauro Correia Silveira <lauro.silveira@outlook.com.br>
2024-01-23 17:16:08 +0100

    Disable CSRF for /tasks only

--- src/main/java/com/lauro/resource/server/config/ResourceServerConfig.java
+++ src/main/java/com/lauro/resource/server/config/ResourceServerConfig.java
@@ -20,7 +20,7 @@ public class ResourceServerConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-                .csrf(csrf -> csrf.configure(csrf.disable()))
+                .csrf(csrf -> csrf.ignoringRequestMatchers("/tasks/**"))
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.decoder(JwtDecoders.fromOidcIssuerLocation(issuerUri))))
                 .build();
     }
